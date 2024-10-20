2b4a3529ddceb0edcf3f526b2e3a433afe51963a
Urban Sanden <hej@urre.me>
2024-10-20 13:07:13 +0200

    Tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -219,122 +219,121 @@ class App extends React.Component {
             </button>
           </div>
           <aside
-            className={`aside ${this.state.sidebar ? "aside-active" : null}`}
+            className={`aside`}
+            aria-expanded={`${this.state.sidebar ? "true" : false}`}
           >
-            <div className="aside-inner">
-              <div className="controls">
-                <button className="button" onClick={this.toggleSidebar}>
-                  <CogIcon />
-                  Close controls
-                </button>
-              </div>
-              <ul>
-                <li>
-                  <label htmlFor="href">
-                    <svg
-                      viewBox="0 0 15 15"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="15"
-                      height="15"
-                      style={{ marginRight: ".5rem" }}
-                    >
-                      <path
-                        d="M4.5 5.5V5a.5.5 0 00-.5.5h.5zm0 1l.354.354L5 6.707V6.5h-.5zm-1.707.293l-.354.353.354-.353zM6.5 13H7v-.207l-.146-.147L6.5 13zm-1-1H5v.207l.146.147L5.5 12zm0-1.5H6v-.207l-.146-.147-.354.354zm-1-1H4v.207l.146.147L4.5 9.5zm0-1V8a.5.5 0 00-.5.5h.5zM9 .5v2h1v-2H9zM8.5 3h-1v1h1V3zm-3 2h-1v1h1V5zM4 5.5v1h1v-1H4zm.146.646l-.292.293.707.707.293-.292-.708-.708zm-1 .293L1.354 4.646l-.708.708L2.44 7.146l.707-.707zM6 4.5a.5.5 0 01-.5.5v1A1.5 1.5 0 007 4.5H6zM7.5 3A1.5 1.5 0 006 4.5h1a.5.5 0 01.5-.5V3zM3.854 6.44a.5.5 0 01-.708 0l-.707.706a1.5 1.5 0 002.122 0l-.707-.707zM9 2.5a.5.5 0 01-.5.5v1A1.5 1.5 0 0010 2.5H9zm-2 12V13H6v1.5h1zm-.146-1.854l-1-1-.708.708 1 1 .708-.708zM6 12v-1.5H5V12h1zm-.146-1.854l-1-1-.708.708 1 1 .708-.708zM5 9.5v-1H4v1h1zM4.5 9h4V8h-4v1zm4.5.5v.667h1V9.5H9zm1.833 2.5H13.5v-1h-2.667v1zM10 11.167c0 .46.373.833.833.833v-1c.092 0 .167.075.167.167h-1zM9.833 11c.092 0 .167.075.167.167h1C11 10.522 10.478 10 9.833 10v1zM9 10.167c0 .46.373.833.833.833v-1c.092 0 .167.075.167.167H9zM8.5 9a.5.5 0 01.5.5h1A1.5 1.5 0 008.5 8v1zm-1 5A6.5 6.5 0 011 7.5H0A7.5 7.5 0 007.5 15v-1zM14 7.5A6.5 6.5 0 017.5 14v1A7.5 7.5 0 0015 7.5h-1zM7.5 1A6.5 6.5 0 0114 7.5h1A7.5 7.5 0 007.5 0v1zm0-1A7.5 7.5 0 000 7.5h1A6.5 6.5 0 017.5 1V0z"
-                        fill="currentColor"
-                      ></path>
-                    </svg>
-                    URL
-                  </label>
-                  <input
-                    type="text"
-                    id="href"
-                    defaultValue={this.state.link.href}
-                    onChange={this.onChange}
-                    autoFocus
-                  />
-                  <p>
-                    Contains a URL or a URL fragment that the hyperlink points
-                    to. URLs are not restricted to Web (HTTP)-based documents,
-                    but can use any protocol supported by the browser. For
-                    example, file:, ftp:, and mailto: work in most browsers.
-                    When linking a URL, consider users who must speak it out
-                    loud and who must listen to a screen reader announce it.{" "}
-                  </p>
-                </li>
-                <li>
-                  <label htmlFor="text">
-                    <svg
-                      viewBox="0 0 15 15"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="15"
-                      height="15"
-                      style={{ marginRight: ".5rem" }}
-                    >
-                      <path
-                        d="M7.5 14V1.5M1.5 5V1.5h12V5m-10 8.5H11"
-                        stroke="currentColor"
-                      ></path>
-                    </svg>
-                    Link text
-                  </label>
-                  <textarea
-                    type="text"
-                    id="text"
-                    defaultValue={this.state.link.text}
-                    onChange={this.onChange}
-                  />
-                  <p>
-                    Link text is the clickable word or phrase in a hyperlink.
-                    When link text clearly conveys a hyperlink's target, both
-                    users and search engines can more easily understand your
-                    content and how it relates to other pages.
-                  </p>
-                </li>
-                <li>
-                  <label htmlFor="class">
-                    <svg
-                      viewBox="0 0 17 16"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="15"
-                      height="15"
-                      style={{ marginRight: ".5rem" }}
-                    >
-                      <path
-                        d="M5 3.5h6.5v3M7 6.5h4.5m0 0v3l-3 1-3-1V8M1.5.5h14l-1 12-6 2-6-2-1-12z"
-                        stroke="currentColor"
-                      ></path>
-                    </svg>
-                    CSS Class
-                  </label>
-                  <input
-                    type="text"
-                    id="class"
-                    defaultValue={this.state.link.class}
-                    onChange={this.onChange}
-                  />
-                  <p>
-                    The class selector selects elements with a specific class
-                    attribute. Separate classes with space.
-                  </p>
-                </li>
-              </ul>
-              <Target useAttribute={this.setAttribute} />
-              <Download useAttribute={this.setAttribute} />
-              <QueryString
-                useAttribute={this.setAttribute}
-                appendQueryString={this.appendQueryString}
-              />
-              <Analytic
-                useAttribute={this.setAttribute}
-                appendUTM={this.appendUTM}
-              />
-              <Rel useAttribute={this.setAttribute} />
-              <ReferrerPolicy useAttribute={this.setAttribute} />
-              <A11y useAttribute={this.setAttribute} />
+            <div className="controls">
+              <button className="button" onClick={this.toggleSidebar}>
+                <CogIcon />
+                Close controls
+              </button>
             </div>
+            <ul>
+              <li>
+                <label htmlFor="href">
+                  <svg
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    style={{ marginRight: ".5rem" }}
+                  >
+                    <path
+                      d="M4.5 5.5V5a.5.5 0 00-.5.5h.5zm0 1l.354.354L5 6.707V6.5h-.5zm-1.707.293l-.354.353.354-.353zM6.5 13H7v-.207l-.146-.147L6.5 13zm-1-1H5v.207l.146.147L5.5 12zm0-1.5H6v-.207l-.146-.147-.354.354zm-1-1H4v.207l.146.147L4.5 9.5zm0-1V8a.5.5 0 00-.5.5h.5zM9 .5v2h1v-2H9zM8.5 3h-1v1h1V3zm-3 2h-1v1h1V5zM4 5.5v1h1v-1H4zm.146.646l-.292.293.707.707.293-.292-.708-.708zm-1 .293L1.354 4.646l-.708.708L2.44 7.146l.707-.707zM6 4.5a.5.5 0 01-.5.5v1A1.5 1.5 0 007 4.5H6zM7.5 3A1.5 1.5 0 006 4.5h1a.5.5 0 01.5-.5V3zM3.854 6.44a.5.5 0 01-.708 0l-.707.706a1.5 1.5 0 002.122 0l-.707-.707zM9 2.5a.5.5 0 01-.5.5v1A1.5 1.5 0 0010 2.5H9zm-2 12V13H6v1.5h1zm-.146-1.854l-1-1-.708.708 1 1 .708-.708zM6 12v-1.5H5V12h1zm-.146-1.854l-1-1-.708.708 1 1 .708-.708zM5 9.5v-1H4v1h1zM4.5 9h4V8h-4v1zm4.5.5v.667h1V9.5H9zm1.833 2.5H13.5v-1h-2.667v1zM10 11.167c0 .46.373.833.833.833v-1c.092 0 .167.075.167.167h-1zM9.833 11c.092 0 .167.075.167.167h1C11 10.522 10.478 10 9.833 10v1zM9 10.167c0 .46.373.833.833.833v-1c.092 0 .167.075.167.167H9zM8.5 9a.5.5 0 01.5.5h1A1.5 1.5 0 008.5 8v1zm-1 5A6.5 6.5 0 011 7.5H0A7.5 7.5 0 007.5 15v-1zM14 7.5A6.5 6.5 0 017.5 14v1A7.5 7.5 0 0015 7.5h-1zM7.5 1A6.5 6.5 0 0114 7.5h1A7.5 7.5 0 007.5 0v1zm0-1A7.5 7.5 0 000 7.5h1A6.5 6.5 0 017.5 1V0z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                  URL
+                </label>
+                <input
+                  type="text"
+                  id="href"
+                  defaultValue={this.state.link.href}
+                  onChange={this.onChange}
+                  autoFocus
+                />
+                <p>
+                  Contains a URL or a URL fragment that the hyperlink points to.
+                  URLs are not restricted to Web (HTTP)-based documents, but can
+                  use any protocol supported by the browser. For example, file:,
+                  ftp:, and mailto: work in most browsers. When linking a URL,
+                  consider users who must speak it out loud and who must listen
+                  to a screen reader announce it.{" "}
+                </p>
+              </li>
+              <li>
+                <label htmlFor="text">
+                  <svg
+                    viewBox="0 0 15 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    style={{ marginRight: ".5rem" }}
+                  >
+                    <path
+                      d="M7.5 14V1.5M1.5 5V1.5h12V5m-10 8.5H11"
+                      stroke="currentColor"
+                    ></path>
+                  </svg>
+                  Link text
+                </label>
+                <textarea
+                  type="text"
+                  id="text"
+                  defaultValue={this.state.link.text}
+                  onChange={this.onChange}
+                />
+                <p>
+                  Link text is the clickable word or phrase in a hyperlink. When
+                  link text clearly conveys a hyperlink's target, both users and
+                  search engines can more easily understand your content and how
+                  it relates to other pages.
+                </p>
+              </li>
+              <li>
+                <label htmlFor="class">
+                  <svg
+                    viewBox="0 0 17 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    style={{ marginRight: ".5rem" }}
+                  >
+                    <path
+                      d="M5 3.5h6.5v3M7 6.5h4.5m0 0v3l-3 1-3-1V8M1.5.5h14l-1 12-6 2-6-2-1-12z"
+                      stroke="currentColor"
+                    ></path>
+                  </svg>
+                  CSS Class
+                </label>
+                <input
+                  type="text"
+                  id="class"
+                  defaultValue={this.state.link.class}
+                  onChange={this.onChange}
+                />
+                <p>
+                  The class selector selects elements with a specific class
+                  attribute. Separate classes with space.
+                </p>
+              </li>
+            </ul>
+            <Target useAttribute={this.setAttribute} />
+            <Download useAttribute={this.setAttribute} />
+            <QueryString
+              useAttribute={this.setAttribute}
+              appendQueryString={this.appendQueryString}
+            />
+            <Analytic
+              useAttribute={this.setAttribute}
+              appendUTM={this.appendUTM}
+            />
+            <Rel useAttribute={this.setAttribute} />
+            <ReferrerPolicy useAttribute={this.setAttribute} />
+            <A11y useAttribute={this.setAttribute} />
           </aside>
           <section>
             <div className="main-html-header">
--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
-export const DEFAULT_ARIA_LABEL = "Make a link and use on the Internet";
-export const DEFAULT_ACCESSKEY = "a";
-export const DEFAULT_TABINDEX = "1";
-export const DEFAULT_ARIA_LABEL_ID = "link-primer";
-export const DEFAULT_ARIA_DESCRIBE_ID = "link-primer";
+export const DEFAULT_ARIA_LABEL = "Make a hyperlink and use on the Internet"
+export const DEFAULT_ACCESSKEY = "a"
+export const DEFAULT_TABINDEX = "1"
+export const DEFAULT_ARIA_LABEL_ID = "make-hyperlink"
+export const DEFAULT_ARIA_DESCRIBE_ID = "make-hyperlink"
 
 export const notes = {
   noreferrer:
@@ -53,4 +53,4 @@ export const notes = {
   querystring_example: "For example: /?ref=producthunt",
   querystring:
     "A query string is a part of a uniform resource locator (URL) that assigns values to specified parameters. A query string commonly includes fields added to a base URL by a Web browser or other client application, for example as part of an HTML document, choosing the appearance of a page, or jumping to positions in multimedia content",
-};
+}
